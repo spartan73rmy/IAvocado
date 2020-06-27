3639ab6a12f4bf1787a9a114419b09708e37b236
José Alberto Espinoza Morelos <spartan73rmy12@gmail.com>
2020-06-27 15:09:05 -0500

    Comentario

--- renameImages.py
+++ renameImages.py
@@ -4,3 +4,5 @@ path='Images'
 imagesToRename = os.listdir(path)
 for i,file in enumerate(imagesToRename):
   os.rename(os.path.join(path, file), os.path.join(path, ''.join([str(i), '.jpg'])))
+
+#Comentario
