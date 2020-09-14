9b5768c015bdd6f10ae4aeed4ca8d59ca7cea5ea
José Alberto Espinoza Morelos <spartan73rmy12@gmail.com>
2020-09-13 22:50:28 -0500

    Imagenes

--- renameImages.py
+++ renameImages.py
@@ -1,7 +1,7 @@
 #Rename images, change path in it 
 
 import os
-path='avocados/aguacate2'
+path='testing/'
 imagesToRename = os.listdir(path)
 for i,file in enumerate(imagesToRename):
   os.rename(os.path.join(path, file), os.path.join(path, ''.join([str(i), '.jpg'])))
